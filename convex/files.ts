350c69bbb000fcda4f598e02f48e18baef8eb96e
Nguyen Xuan Duong <20521220@gm.uit.edu.vn>
2024-07-09 15:02:45 +0700

    add ablility to delete files

--- convex/files.ts
+++ convex/files.ts
@@ -82,3 +82,32 @@ export const getFiles = query({
       .collect();
   },
 });
+
+export const deleteFile = mutation({
+  args: { fileId: v.id("files") },
+  async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("You must be logged in to upload a file");
+    }
+
+    const file = await ctx.db.get(args.fileId)
+
+    if (!file) {
+      throw new ConvexError("This file does not exist")
+    }
+
+    const hasAccess = await hasAccessToOrg(
+      ctx,
+      identity.tokenIdentifier,
+      file.orgId
+    );
+
+    if (!hasAccess) {
+      throw new ConvexError("You do not have access to delete this file");
+    }
+    
+    await ctx.db.delete(args.fileId)
+  },
+});
