5684d9547f08d602e44f550f6daba3413e561c52
Nguyen Xuan Duong <20521220@gm.uit.edu.vn>
2024-07-18 07:57:05 +0700

    fix landing page

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -11,7 +11,7 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <div className="border-b py-1 bg-gray-50">
+    <div className="relative z-10 border-b py-1 bg-gray-50">
       <div className="flex items-center justify-between px-4">
         <Link href="/" className="flex gap-2 items-center text-xl">
           {" "}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function LandingPage() {
   return (
     <div className="bg-white">
@@ -14,9 +17,15 @@ export default function LandingPage() {
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
           />
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
+        <div className="mx-auto max-w-2xl py-8">
           <div className="text-center">
+            <Image
+              src="/logo.png"
+              width="200"
+              height="200"
+              alt="file drive logo"
+              className="inline-block mb-8"
+            />
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
               The easiest way to upload and share files with your company
             </h1>
@@ -25,12 +34,12 @@ export default function LandingPage() {
               minute.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
+              <Link
+                href="/dashboard/files"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
-              </a>
+              </Link>
               <a
                 href="#"
                 className="text-sm font-semibold leading-6 text-gray-900"
